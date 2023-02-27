35b7ec361ab26d07cfc812b86c5fdff62e6245bd
Giancarlo Fontela da Luz <gian.f.luz@hotmail.com>
2023-02-27 12:38:27 -0300

    test: Lib test coverage

--- index.test.ts
+++ index.test.ts
@@ -1,3 +1,4 @@
+import PathTree from './src/interfaces/PathTree';
 import { test, expect } from '@jest/globals';
 
 import resolver from './index';
@@ -6,12 +7,91 @@ test('Instance check', () => {
     expect(resolver).toBeInstanceOf(Function);
 });
 
-test('Vibe check', () => {
-    const t = (resolver([
-        '/test',
-        '/test/a/b/c',
-        '/test/a/b/d',
-    ]));
+test('Empty resolve', () => {
+    const emptyTree: PathTree<never> = {
+        self: '/',
+        children: {}
+    };
 
-    console.dir(t, { depth: null });
+    expect(resolver([])).toEqual(emptyTree);
 });
+
+test('Invalid resolves', () => {
+    expect(() => resolver(['a', 'b', 'c'])).toThrowError(
+        new Error('Path must start with /')
+    );
+
+    expect(() => resolver(['/', '/'])).toThrowError(
+        new Error('Duplicate path found')
+    );
+});
+
+test('Valid resolves', () => {
+    const simpleTree: PathTree<never> = {
+        self: '/',
+        children: {
+            a: {
+                self: '/a',
+                children: {}
+            },
+            b: {
+                self: '/b',
+                children: {
+                    c: {
+                        self: '/b/c',
+                        children: {}
+                    }
+                }
+            }
+        }
+    };
+
+    const complexTree: PathTree<number> = {
+        self: '/',
+        payload: 0,
+        children: {
+            a: {
+                self: '/a',
+                payload: 1,
+                children: {}
+            },
+            b: {
+                self: '/b',
+                payload: 2,
+                children: {
+                    c: {
+                        self: '/b/c',
+                        payload: 3,
+                        children: {}
+                    }
+                }
+            }
+        }
+    };
+
+    const simplePathList = ['/a', '/b/c'];
+    const complexPathList = [
+        {
+            path: '/',
+            payload: 0
+        },
+        {
+            path: '/a',
+            payload: 1
+        },
+        {
+            path: '/b',
+            payload: 2
+        },
+        {
+            path: '/b/c',
+            payload: 3
+        }
+    ];
+
+    expect(resolver(simplePathList)).toEqual(simpleTree);
+
+    expect(resolver(complexPathList)).toEqual(complexTree);
+});
+
+test('Vibe check', () => {});
