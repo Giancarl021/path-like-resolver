cfb02956c295286118e19c96e7d0226b3a8f4fca
Giancarlo Fontela da Luz <gian.f.luz@hotmail.com>
2023-02-27 09:33:23 -0300

    build: Recursion

--- index.test.ts
+++ index.test.ts
@@ -6,6 +6,12 @@ test('Instance check', () => {
     expect(resolver).toBeInstanceOf(Function);
 });
 
-test('', () => {
-    
+test('Vibe check', () => {
+    const t = (resolver([
+        '/test',
+        '/test/a/b/c',
+        '/test/a/b/d',
+    ]));
+
+    console.dir(t, { depth: null });
 });
--- src/services/parse.ts
+++ src/services/parse.ts
@@ -1,18 +1,39 @@
 import { InternalPathNode, PathTree } from '@interfaces';
 
-export default function <T>(
+function fillBranch<T>(currentTree: PathTree<T>, child: InternalPathNode<T>) {
+    if (currentTree.self === child.path) {
+        currentTree.payload = child.payload;
+        return;
+    }
+
+    const directChild = child.parsedPath[0];
+
+    if (!directChild) return;
+
+    if (!currentTree.children[directChild]) {
+        currentTree.children[directChild] = {
+            self:
+                currentTree.self +
+                (currentTree.self === '/' ? directChild : ('/' + directChild)),
+            children: {},
+            payload: undefined
+        };
+    }
+
+    const parsedChild = { ...child, parsedPath: child.parsedPath.slice(1) };
+
+    fillBranch(currentTree.children[directChild], parsedChild);
+}
+
+export default function parse<T>(
     nodes: InternalPathNode<T>[],
-    currentTree: PathTree<T>
+    tree: PathTree<T>
 ): PathTree<T> {
     const sortedNodes = nodes.sort((a, b) => a.path.length - b.path.length);
 
     for (const node of sortedNodes) {
-        if (!node.parsedPath.length) {
-            currentTree.payload = node.payload;
-        }
-
-        
+        fillBranch(tree, node);
     }
 
-    return {} as PathTree<T>;
+    return tree;
 }
